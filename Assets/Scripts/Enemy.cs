1221322d80faa982f7b36c769eb695654fb6bba8
Zlobik <60093914+Zlobik@users.noreply.github.com>
2020-12-11 12:40:26 +0300

    RefactoredCode

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed = 2;
+    [SerializeField] private bool _isGiveSpeed;
+    [SerializeField] private float _boosterForce;
+    [SerializeField] private float _boosterTime;
+
+    private Vector3 _target;
+
+    public bool IsGiveSpeed => _isGiveSpeed;
+    public float BoosterForce => _boosterForce;
+    public float BoosterTime => _boosterTime;
+
+    private void OnTriggerEnter2D (Collider2D collision)
+    {
+        if (collision.GetComponent<Player>())
+            gameObject.SetActive(false);
+    }
+
+    private void Start ()
+    {
+        _target = Random.insideUnitCircle * 4;
+    }
+
+    private void Update ()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+
+        if (transform.position == _target)
+            _target = Random.insideUnitCircle * 4;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public int KilledEnemys { get; private set; } = 0;
+
+    private void OnTriggerEnter2D (Collider2D collision)
+    {
+        if(collision.TryGetComponent(out Enemy enemy))
+            KilledEnemys++;
+    }
+}
